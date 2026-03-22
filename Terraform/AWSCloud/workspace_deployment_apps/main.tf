b40d265e1aed6084fc5a9c84ba7a3f3bedd47d66
erosons <eromonsei.o.samson@gmail.com>
2026-03-22 01:07:54 +0000

    updates

--- Terraform/AWSCloud/workspace_deployment_apps/main.tf
+++ Terraform/AWSCloud/workspace_deployment_apps/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "network" {
-  source                 = "./modules/network"
+  source                 = "../modules/network"
   # workspace vpc
   vpc_cidr               = "10.10.0.0/16"
   ws_private_subnet_cidr = "10.10.1.0/24"
@@ -25,7 +25,7 @@ module "dns" {
 }
 
 module "private_endpoints" {
-  source = "./modules/private_endpoints"
+  source = "../modules/private_endpoints"
 
   # Back-end in workspace VPC
   workspace_vpc_id        = module.network.vpc_id
@@ -45,7 +45,7 @@ module "private_endpoints" {
 }
 
 module "workspace" {
-  source = "./modules/databricks_workspace"
+  source = "../modules/workspace_module"
 
   # Databricks account-level (E2) wiring
   account_id                  = var.databricks_account_id
@@ -72,42 +72,9 @@ module "workspace" {
 
 # Make the A & CNAME records that Databricks requires for front-end PL
 module "dns_records" {
-  source              = "./modules/dns/records"
+  source              = "./modules/dns"
   zone_id             = module.dns.zone_id
   workspace_hostname  = module.workspace.workspace_hostname  # <deployment>.cloud.databricks.com
   workspace_id        = module.workspace.workspace_id        # dbc-dp-<workspace-id>
   frontend_vpce_ip    = module.private_endpoints.frontend_vpce_ip
-}
-
-module "dbx_workspace" {
-  source   = "../workspace_module"
-  for_each = toset(local.environments)
-
-   # 👇 child uses account-level resources with provider = databricks.mws
-   providers = {
-    databricks.mws = databricks.mws
-  }
-
-    aws_region                    = "us-east-1"
-    workspace_name_prefix         = "dbx-databricks-wk-${each.key}"
-    access_connector_prefix       = "dbx-access-connector-${each.key}"
-    bucket_name_prefix            = "dbx-bucket-${each.key}"
-    tf_backend_bucket             = "dbx-backend-tf-${each.key}"
-    dynamodb_table_name           = "dbx-remote-state-lock"
-    databricks_account_id         = var.databricks_account_id
-    vpc_id                        = "vpc-0f80ef0a48b036fe8"
-    network_prefix                = "dbx-network-${each.key}"
-  subnets_private_ids           = ["subnet-0f0beef3de5e7fa66","subnet-058abd6ad47cdfe3c"]
-  security_group_ids            = ["sg-0c04dbd290b14cf7c"]
-  name                          = "dbx-workspace-${each.key}"
-    client_id                     = var.client_id
-    client_secret                 = var.client_secret
-    //aad_groups              = ["account_unity_admin", "data_engineer", "data_analyst", "data_scientist"]
-    tags = {
-    "Project" = "POC"
-    "Environment" = each.key
-    "Owner" = "Samson"
-    }
-
-
 }
